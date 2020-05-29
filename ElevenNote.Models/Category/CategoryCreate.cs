a5ad3e7d0f7cb84ecfb8ac33f48a874de5cafd40
Fletcher Kitchell <fdkitchell@gmail.com>
2020-05-29 16:31:01 -0400

    added scaffold for category manipulation

---
+++ ElevenNote.Models/Category/CategoryCreate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Models.Category
+{
+    class CategoryCreate
+    {
+
+        [Required]
+        public Guid OwnerId { get; set; }
+        [Required]
+        public string CategoryTitle { get; set; }
+    }
+}
